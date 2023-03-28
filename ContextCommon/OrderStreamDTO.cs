2e12d0a76b218774d4ce8ef3801ba02e92a5248d
ndicz <hnrmanesi@gmail.com>
2023-03-28 09:03:36 +0700

    Add project files.

---
+++ ContextCommon/OrderStreamDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContextCommon
+{
+    public class OrderStreamDTO
+    {
+    }
+}
---
+++ ContextCommon/SalesStreamDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContextCommon
+{
+    public class SalesStreamDTO
+    {
+        public s
+    }
+}
